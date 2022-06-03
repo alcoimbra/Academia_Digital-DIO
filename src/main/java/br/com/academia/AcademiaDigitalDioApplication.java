d9d7e433c45d978e4192fbbb9da88740c66ea8d4
Alan Coimbra <algcoimbra01@gmail.com>
2022-06-03 11:33:27 -0300

    Iniciando o Projeto Academia

---
+++ src/main/java/br/com/academia/AcademiaDigitalDioApplication.java
@@ -0,0 +1,13 @@
+package br.com.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDigitalDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaDigitalDioApplication.class, args);
+	}
+
+}
